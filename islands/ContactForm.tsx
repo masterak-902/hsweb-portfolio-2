d12188557de195cd524cea0a7f122b4914c912b3
Ubuntu HT <hsn-software@masterak.jp>
2024-07-24 19:30:27 +0900

    refactor: Update Button component styles and add ContactForm to home page

--- islands/ContactForm.tsx
+++ islands/ContactForm.tsx
@@ -2,7 +2,7 @@ import { Button } from "../components/Button.tsx";
 
 export default function ContactForm() {
     return (
-        <main class="bg-slate-100 mt-8 py-6 px-8 max-w-[760px] h-auto mx-auto">
+        <main class="bg-slate-100 mt-8 py-6 px-8 max-w-[760px] h-auto mx-auto rounded">
             <h2 class="text-2xl mb-4 text-center font-bold">お問い合わせフォーム</h2>
             <form class="flex flex-col">
                 <label class="mb-2" for="email">メールアドレス</label>
--- routes/index.tsx
+++ routes/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <section>
       <div class="px-4 py-8 mx-auto bg-[#86efac]">
         <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-          <h1 class="text-4xl font-bold">Welcome to our contact page.</h1>
+          <h1 class="text-4xl tracking-wider font-bold">Welcome to our contact page.</h1>
           <p class="my-4">
           If you have any requests, please mention them in the form below.
           </p>
