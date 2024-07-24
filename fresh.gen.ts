416857a9f3a5dbc77c6fa0fa87b2f359d3ea0c94
Ubuntu HT <hsn-software@masterak.jp>
2024-07-24 14:14:15 +0900

    chore: Update Button component styles and add ContactForm to home page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,6 +7,7 @@ import * as $_app from "./routes/_app.tsx";
 import * as $api_joke from "./routes/api/joke.ts";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
+import * as $ContactForm from "./islands/ContactForm.tsx";
 import * as $Counter from "./islands/Counter.tsx";
 import { type Manifest } from "$fresh/server.ts";
 
@@ -19,6 +20,7 @@ const manifest = {
     "./routes/index.tsx": $index,
   },
   islands: {
+    "./islands/ContactForm.tsx": $ContactForm,
     "./islands/Counter.tsx": $Counter,
   },
   baseUrl: import.meta.url,
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,12 +1,17 @@
+import ContactForm from "../islands/ContactForm.tsx";
+
 export default function Home() {
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <h1 class="text-4xl font-bold">Welcome to Login Test Page</h1>
-        <p class="my-4">
-          Add /login to the end of the URL or press the button below!
-        </p>
+    <section>
+      <div class="px-4 py-8 mx-auto bg-[#86efac]">
+        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+          <h1 class="text-4xl font-bold">Welcome to our contact page.</h1>
+          <p class="my-4">
+          If you have any requests, please mention them in the form below.
+          </p>
+        </div>
       </div>
-    </div>
+      <ContactForm />
+    </section>
   );
 }
