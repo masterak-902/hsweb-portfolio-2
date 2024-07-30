6ee389197833c7a94b5380f7ca6b8811b1791b54
masterak-902 <hsn-software@masterak.jp>
2024-07-30 15:35:50 +0900

    refactor: Update contact form with reCAPTCHA integration

--- islands/LoginForm.tsx
+++ islands/LoginForm.tsx
@@ -1,6 +1,10 @@
 import { Button } from "../components/Button.tsx";
 
 export default function Login() {
+
+    // Turnstile documentation(client code)
+    // https://developers.cloudflare.com/turnstile/get-started/client-side-rendering/
+
     return (
         <form method="post" action="/api/login">
             
@@ -10,7 +14,7 @@ export default function Login() {
                 <input class="mb-4 p-2 border-2 rounded-md" type="text" name="username" placeholder="ユーザーIDを入力してください" /><br />
                 <input class="mb-4 p-2 border-2 rounded-md" type="password" name="password" placeholder="パスワードを入力してください" />
             </div>
-
+            
             <Button>ログイン</Button>
             
         </form>
--- routes/api/post.ts
+++ routes/api/post.ts
@@ -15,31 +15,66 @@ export const handler: Handlers = {
     async POST(req: Request) {
     
         const form = await req.formData();
-        const json = JSON.stringify(Object.fromEntries(form.entries()));
 
-        console.log(json);
-        const headers = new Headers();
-        const apikey = Deno.env.get('POST_API_KEY');
-        console.log(apikey);
-    
-        headers.set('X-API-KEY', apikey ?? '');
-        headers.set('Content-Type', 'application/json');
+        // Turnstile injects a token in "cf-turnstile-response".
+        const turnstile_token = form.get('cf-turnstile-response');
+        const ip = req.headers.get('CF-Connecting-IP');
 
-        // Hono API(/sender)へfetchの実行
-        const response = await fetch('http://localhost:8787/sender', {
+        const verification_formData = new FormData();
+        verification_formData.append('secret', Deno.env.get('RECAPTCHA_SECRET_KEY') ?? '');
+        verification_formData.append('response', turnstile_token ?? '');
+        verification_formData.append('remoteip', ip ?? '');
+
+        const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
+        const result = await fetch(url, {
+            body: verification_formData,
             method: 'POST',
-            headers,
-            body: json,
         });
         
-        if (response.ok) {
-            const responseData = await response.json();
-            console.log(responseData);
-            return new Response(null, {});
+        const outcome = await result.json();
+        let responseData;
+        if (outcome.success) {
+            // 1. form.entries(): retrieve all entries (key/value pairs) of a form object.
+            // 2. Object.fromEntries(form.entries()): converts the retrieved entries into objects.
+            // 3. JSON.stringify(Object.fromEntries(form.entries())): converts the object into a string in JSON format.
+            const json = JSON.stringify(Object.fromEntries(form.entries()));
+
+            const headers = new Headers();
+            const apikey = Deno.env.get('POST_API_KEY');
+        
+            headers.set('X-API-KEY', apikey ?? '');
+            headers.set('Content-Type', 'application/json');
+
+            // Hono API(/sender)へfetchの実行
+            const response = await fetch('http://localhost:8787/sender', {
+                method: 'POST',
+                headers,
+                body: json,
+            });
+            
+            try {
+                responseData = await response.json();
+                console.log(responseData);
+                return new Response(JSON.stringify(responseData), {
+                    headers: { 'Content-Type': 'application/json' },
+                });
+                // return new Response(null, {});
+
+            // deno-lint-ignore no-unused-vars
+            } catch (error) {
+                // console.error(error);
+                // return new Response(error);
+                return new Response(null, {'status': 403});
+            }
         } else {
-            const responseData = await response.json();
-            console.log(responseData);
+            console.error('Error:', outcome['error-codes']);
+            // responseData = { isSuccessful: false,  message: 'トークンの有効期限が切れているまたは、認証に失敗しました。'};
+            // return new Response(JSON.stringify(responseData), {
+            //     headers: { 'Content-Type': 'application/json' },
+            // });
             return new Response(null, {});
         }
-    }
+
+    }  
 }
+
