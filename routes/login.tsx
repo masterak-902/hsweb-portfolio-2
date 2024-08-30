e0f64b0463006624ce319f2cd80da79a7b95a21a
masterak-902 <hsn-software@masterak.jp>
2024-08-30 12:27:40 +0900

    refactor: Remove commented out code and update login form UI

--- routes/login.tsx
+++ routes/login.tsx
@@ -1,4 +1,4 @@
-import { Button } from '../components/Button.tsx';
+// import { Button } from '../components/Button.tsx';
 
 // 1. Verify that you have valid session storage
 // import type { Handlers } from '$fresh/server.ts';
@@ -22,14 +22,14 @@ import { Button } from '../components/Button.tsx';
 export default function Home() {
     return(
         <div class='py-8 px-12'>
-            <form method='post' >
+            {/* <form method='post' >
                 <h2 class='mb-4 text-2xl'>ログイン画面</h2>
                 <div>
                     <input class='w-[300px] mb-4 p-2 border-2 rounded-md' type='text' name='username' placeholder='ユーザーIDを入力してください' /><br />
                     <input class='w-[300px] mb-4 p-2 border-2 rounded-md' type='password' name='password' placeholder='パスワードを入力してください' />
                 </div>
                 <Button>ログイン</Button>
-            </form>
+            </form> */}
             <p class='text-red-500'>ログインに失敗しました。 再度お試しください。</p>
             <div class='mt-8'>
                 <h3 class='text-xl font-bold mb-2'>ゲストアカウント</h3>
